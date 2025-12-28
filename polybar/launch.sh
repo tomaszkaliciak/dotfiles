a2c2c8db010b6295fbb8b98515ccea0f945b6ddd
Tomasz Kaliciak <tomasz.kaliciak1@gmail.com>
2025-12-28 15:17:34 +0100

    update dotfiles

--- polybar/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-polybar-msg cmd quit
-# killall polybar
-
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-polybar main
--- polybar/task.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-out=$(/home/tomasz/Programming/rust/tmc/target/debug/tmc current)
-echo $out
